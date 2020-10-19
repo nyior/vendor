df0d4ff603b68b6474ac0ddb844340328183c91a
Nyior Clement Jr <cnyior27@gmail.com>
2020-10-19 16:13:37 +0100

    change static files dir

--- marche/settings.py
+++ marche/settings.py
@@ -221,7 +221,7 @@ MEDIA_URL  = '/media/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
     os.path.join(FRONTEND_DIR, 'dist'),
 ]
 
