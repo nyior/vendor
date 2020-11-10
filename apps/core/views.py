3ab103a57b21ed7cc9a8f750c960edeed9200044
Nyior Clement Jr <cnyior27@gmail.com>
2020-11-10 14:54:05 +0100

    preparing app for digital ocean deployment

--- apps/core/views.py
+++ apps/core/views.py
@@ -5,7 +5,7 @@ class IndexView(TemplateView):
     
     def get_template_names(self):
 
-        if settings.DEBUG:
+        if not settings.DEBUG:
             template_name = 'index-dev.html'
         else:
             template_name = 'index.html'
---
+++ static/precache-manifest.c6df09f602c6cda8244b10a84b5a4957.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcad519bc66dcad94607a68b2fbedfce",
+    "url": "http://127.0.0.1:8080/index.html"
+  },
+  {
+    "revision": "3846ad6f6fa528410b7a",
+    "url": "http://127.0.0.1:8080/bundle.js"
+  },
+  {
+    "revision": "3846ad6f6fa528410b7a",
+    "url": "http://127.0.0.1:8080/bundle.css"
+  }
+];
