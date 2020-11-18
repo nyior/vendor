b9c1e3352592a5352c2607e56c1fcbd6fc3ce88c
Nyior Clement Jr <cnyior27@gmail.com>
2020-11-18 14:26:00 +0100

    moved static files

--- marche/settings.py
+++ marche/settings.py
@@ -196,9 +196,9 @@ MEDIA_URL  = '/media/'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/') #comment this when working locally
 
 STATICFILES_DIRS = [
     os.path.join(FRONTEND_DIR, 'dist/'),
-    os.path.join(BASE_DIR, 'static/')
+    # os.path.join(BASE_DIR, 'static/') #uncomment this when working locally 
 ]
