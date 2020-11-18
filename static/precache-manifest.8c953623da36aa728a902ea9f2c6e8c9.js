3bc0a5ed4f303f99bde0e773053dd623000940bc
Nyior Clement Jr <cnyior27@gmail.com>
2020-11-18 14:38:12 +0100

    collected static files

---
+++ static/precache-manifest.8c953623da36aa728a902ea9f2c6e8c9.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcad519bc66dcad94607a68b2fbedfce",
+    "url": "http://127.0.0.1:8080/index.html"
+  },
+  {
+    "revision": "81f3167b5913e57d5cbe",
+    "url": "http://127.0.0.1:8080/bundle.js"
+  },
+  {
+    "revision": "81f3167b5913e57d5cbe",
+    "url": "http://127.0.0.1:8080/bundle.css"
+  }
+];
