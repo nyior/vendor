2d17f69758568d9141af56d3413f1f4cc799e322
Nyior Clement Jr <cnyior27@gmail.com>
2020-12-21 16:42:11 +0100

    docs added

--- apps/adverts/api/pagination.py
+++
@@ -1,4 +0,0 @@
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 18
-    page_size_query_param = 'page_size'
-    max_page_size = 180
--- apps/adverts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdvertsConfig(AppConfig):
-    name = 'adverts'
